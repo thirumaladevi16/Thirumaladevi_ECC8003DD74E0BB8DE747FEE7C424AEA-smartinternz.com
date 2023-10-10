6adf1b5766773f638c6ece3a086bf1a9a8358c5a
thirumaladevi16 <145098123+thirumaladevi16@users.noreply.github.com>
2023-10-10 10:46:44 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,14 +1,19 @@
-
-def factorial(n):
-	
-	if n == 0:
-		return 1
-	
-	return n * factorial(n-1)
-
-
-num = 5;
-print("Factorial of", num, "is",
-factorial(num))
-
+#Define the Player class
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+# Define the Batsman class, derived from Player
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+# Define the Bowler class, derived from Player
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+# Call the play() method for each object
+batsman.play()
+bowler.play()
 
